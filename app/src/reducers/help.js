b472b65b4777652655e049c01e60ec0e5e8c4142
Ryan Collins <rccproductions@mac.com>
2016-07-21 00:35:28 -0400

    Fix: reducer was importing incorrect selectors in help

--- app/src/reducers/help.js
+++ app/src/reducers/help.js
@@ -1,7 +1,18 @@
 import {
   HANDLE_BACKWARD,
   HANDLE_FORWARD
-} from '../actions/help';
+} from '../constants/help';
+
+const stepIndex = (state = 0, action) => {
+  switch (action.type) {
+    case HANDLE_FORWARD:
+      return 2;
+    case HANDLE_BACKWARD:
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 const help = (state = {
   stepIndex: 0
@@ -9,11 +20,11 @@ const help = (state = {
   switch (action.type) {
     case HANDLE_FORWARD:
       return Object.assign({}, state, {
-        stepIndex: 1
+        stepIndex: stepIndex(state.stepIndex, action)
       });
     case HANDLE_BACKWARD:
       return Object.assign({}, state, {
-        stepIndex: 0
+        stepIndex: stepIndex(state.stepIndex, action)
       });
     default:
       return state;
