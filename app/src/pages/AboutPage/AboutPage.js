8789c04ed258fa3b45443ebd6b4030e25784041b
Ryan Collins <rccproductions@mac.com>
2016-07-22 16:34:53 -0400

    Fix: refactor the about page component into class

--- app/src/pages/AboutPage/AboutPage.js
+++ app/src/pages/AboutPage/AboutPage.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import styles from './AboutPage.module.scss';
 import cssModules from 'react-css-modules';
 import { AboutCard } from 'components';
@@ -12,14 +12,11 @@ class AboutPage extends Component {
     super();
     this.handleMouseOver = this.handleMouseOver.bind(this);
     this.handleMouseOut = this.handleMouseOut.bind(this);
-    this.state = {
-      shadowDepth: 1
-    };
   }
   handleMouseOver() {
     console.log(`Mousing over`);
     this.setState({
-      shadowDepth: 4
+      shadowDepth: 5
     });
   }
   handleMouseOut() {
@@ -28,7 +25,7 @@ class AboutPage extends Component {
       shadowDepth: 1
     });
   }
-  return() {
+  render() {
     const {
       shadowDepth
     } = this.state;
@@ -37,14 +34,13 @@ class AboutPage extends Component {
         <Column small={12} medium={12} large={12} className={styles.outer}>
           <AboutCard
             shadowDepth={shadowDepth}
-            onMouseOut={this.handleMouseOut}
             onMouseOver={this.handleMouseOver}
+            onMouseOut={this.handleMouseOut}
           />
         </Column>
       </Row>
     );
   }
 }
-
 
 export default cssModules(AboutPage, styles);
