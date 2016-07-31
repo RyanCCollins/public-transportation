b5ab7edf194f7203a5bb3ee218e9da9ae0ffc5d5
Ryan Collins <rccproductions@mac.com>
2016-07-31 18:32:00 -0400

    Feat: add snackbar to show when app is offline for main.js

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Snackbar } from 'material-ui';
 import { Navbar } from 'containers';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const Main = (props) => (
-  <div>
-    <MuiThemeProvider>
-      <Navbar>
-        {React.cloneElement(props.children, props)}
-      </Navbar>
-    </MuiThemeProvider>
-  </div>
-);
+class Main extends Component {
+  render() {
+    const {
+      isOffline,
+      hasSnackbar
+    } = this.props;
+    return (
+      <div>
+        <Snackbar
+          open={hasSnackbar && isOffline}
+          message={'The application is offline, but will load a default schedule for you!'}
+          autoHideDuration={4000}
+          action="Close"
+          onActionTouchTap={this.handleCloseSnackbar}
+          onRequestClose={this.handleCloseSnackbar}
+        />
+        <MuiThemeProvider>
+          <Navbar>
+            {React.cloneElement(props.children, props)}
+          </Navbar>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
 
-export default Main;
+Main.propTypes = {
+  hasSnackbar: PropTypes.bool.isRequired,
+  isOffline: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => ({
+
+});
+
+export default connect(
+  mapStateToProps
+)(Main);
